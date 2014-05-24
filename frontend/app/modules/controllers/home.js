0e02cf610a746d22ebccf954ab952d1276596a97
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-24 18:27:49 -0300

    Adicionada estrutura para seções dos gráficos

--- frontend/app/modules/controllers/home.js
+++ frontend/app/modules/controllers/home.js
@@ -13,7 +13,12 @@ angular
         url: '^/',
         views: {
           nav: { controller: 'HomeCtrl', templateUrl: 'modules/views/nav.html' },
-          main: { controller: 'HomeCtrl', templateUrl: 'modules/views/home.html' }
+          main: { controller: 'HomeCtrl', templateUrl: 'modules/views/home.html' },
+          footer: { controller: 'HomeCtrl', templateUrl: 'modules/views/footer.html' },
+
+          'pieChart@home': { templateUrl: 'modules/views/piechart.html' },
+          'histogram@home': { templateUrl: 'modules/views/histogram.html' },
+          'tableData@home': { templateUrl: 'modules/views/tabledata.html' }
         }
       });
   });
