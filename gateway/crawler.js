779d856b7e506b858f9c3dba03e9bebebccb19bc
Felipe Mobus <fmobus@gmail.com>
2014-05-24 20:01:59 -0300

    gera os inserts em sql

--- gateway/crawler.js
+++ gateway/crawler.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 var config     = require('./lib/config');
 var connection = require('./lib/services/connection').build(config.api_path);
 
@@ -7,15 +9,47 @@ function identity(v) {
   return v;
 }
 
-function sqlInsert(entry) {
-  return "insert yo mama" + entry.situacao;
+function sqlInsertFor(table) {
+  function quote(value) {
+    if (value === null) {
+      return 'null';
+    }
+    else if (/\d\d\d\d-\d\d-\d\d/.test(value)) {
+      return '"'+value+'"';
+    }
+    else if (parseInt(value)) {
+      return value;
+    }
+    else {
+      return '"'+value+'"';
+    }
+  }
+
+  return function(entry) {
+    var pairs = [];
+
+    _(entry).each(function(value, key) {
+      if (_(value).isObject()) {
+        key   = key + '_id';
+        value = _(value).values()[0].id;
+      }
+      pairs.push([key, value]);
+    });
+
+    var columns = _.chain(pairs).pluck(0).value();
+    var values  = _.chain(pairs).pluck(1).map(quote).value();
+
+    var query = 'insert into ' + table + ' (' + columns.join(',') + ')' +
+                '                   values (' + values.join(',')  + ')';
+    return query;
+  };
 }
 
 function handleError(e) {
   console.log(e);
 }
 
-var p = connection.allPages('/' + endpoint, endpoint, sqlInsert).then(function(result) {
+var p = connection.allPages('/' + endpoint, endpoint, sqlInsertFor(endpoint)).then(function(result) {
   console.log(result);
   console.log('done');
 }).fail(handleError).done(handleError);
