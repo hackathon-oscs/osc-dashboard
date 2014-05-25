757c802ee8c1adb707757a6e518233a42eea78b3
Felipe Mobus <fmobus@gmail.com>
2014-05-25 11:32:45 -0300

    implementado rota de municipio por estado

--- gateway/lib/controllers/queries.js
+++ gateway/lib/controllers/queries.js
@@ -1,3 +1,4 @@
+var Q = require('q');
 var _ = require('underscore');
 var sap = require('sqlite3-as-promised');
 var sqlite3 = require('sqlite3');
@@ -5,8 +6,22 @@ var db = new sqlite3.Database('xunga.db');
 
 var promisedRoute = require('../promised_route');
 module.exports = function(app) {
+  app.get('/municipios/:uf', promisedRoute(function(req, res) {
+    var deferred = Q.defer();
+
+    var stmt = db.prepare('select * from municipios where uf = ?');
+
+    stmt.all([req.params.uf.toUpperCase()], function(err, result) {
+      console.log(result);
+      console.log(stmt);
+      deferred.resolve(result);
+    });
+
+    stmt.finalize();
+
+    return deferred.promise;
+  }));
   app.get('/convenios', promisedRoute(function(req, res) {
-    console.log(sap);
     return sap.all(db, 'select count(1) from convenios');
   }));
 };
