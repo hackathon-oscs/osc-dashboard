76f380d2fdfdf73662b5be8d2d1527bd01738d95
Carlos Henrique Ferreira Oliveira <carlospecter@gmail.com>
2014-05-25 01:15:05 -0300

    Adicionado header com logo

--- frontend/app/modules/controllers/home.js
+++ frontend/app/modules/controllers/home.js
@@ -12,10 +12,12 @@ angular
       .state('home', {
         url: '^/',
         views: {
+          header: { controller: 'HomeCtrl', templateUrl: 'modules/views/header.html' },
           nav: { controller: 'HomeCtrl', templateUrl: 'modules/views/nav.html' },
           main: { controller: 'HomeCtrl', templateUrl: 'modules/views/home.html' },
           footer: { controller: 'HomeCtrl', templateUrl: 'modules/views/footer.html' },
 
+          'alerts@home': { templateUrl: 'modules/views/alerts.html' },
           'pieChart@home': { templateUrl: 'modules/views/piechart.html' },
           'histogram@home': { templateUrl: 'modules/views/histogram.html' },
           'tableData@home': { templateUrl: 'modules/views/tabledata.html' }
